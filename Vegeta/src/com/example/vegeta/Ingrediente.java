9343e97acf8c2dcc18752bae297265b5d9e14070
benja <bcastro@alumnos.inf.utfsm.cl>
2014-10-08 19:13:25 -0300

    asd

--- Vegeta/src/com/example/vegeta/Ingrediente.java
+++ Vegeta/src/com/example/vegeta/Ingrediente.java
@@ -1,11 +1,15 @@
 package com.example.vegeta;
 
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseUser;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Ingrediente extends Activity implements OnClickListener {
 	
@@ -13,6 +17,9 @@ public class Ingrediente extends Activity implements OnClickListener {
 	Button btn9;
 	Button btn10;
 	Intent i;
+	ParseUser pu;
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +27,11 @@ public class Ingrediente extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ingrediente);
 		init();
+		pu = ParseUser.getCurrentUser();
 		
 	}
 	public void init(){
+		
 		btn8 = (Button)findViewById(R.id.button1);
 		btn8.setOnClickListener(this);
 		btn9 = (Button)findViewById(R.id.button2);
@@ -32,17 +41,28 @@ public class Ingrediente extends Activity implements OnClickListener {
 	}
 	@Override
 	public void onClick(View v) {
+		
+	          
 		int id;
 		id= v.getId();
 		switch (id){
 		
 		 	case R.id.button1: //lanzar formulario
+		 		if (pu.equals(null)){
+		 			Toast.makeText(getApplicationContext(),"Inicie sesion para acceder a esta funcionalidad",Toast.LENGTH_LONG).
+		 			show();
+		 		}
+		 		else{
 		 		i = new  Intent(this,FormIngrediente.class);
-		 		startActivity(i);
+		 		startActivity(i);}
 		 		break;
 			case R.id.button2:
+				if (false){
+		 			Toast.makeText(getApplicationContext(),"Inicie sesion para acceder a esta funcionalidad",Toast.LENGTH_LONG).
+		 			show();
+		 		}else{
 				i = new Intent(this,MisIngredientes.class);
-				startActivity(i);
+				startActivity(i);}
 				break;
 			case R.id.button3:
 				i = new Intent(this, BuscarIngredienteActivity.class);
